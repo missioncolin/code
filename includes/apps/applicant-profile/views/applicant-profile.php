b3dbc7312bafa3ae54b79ce6b3ed2dfb67190f81
Jon Rundle <jon@resolutionim.com>
2012-07-23 14:24:32 -0400

    Markup for applicant profile

---
+++ includes/apps/applicant-profile/views/applicant-profile.php
@@ -0,0 +1,79 @@
+<section id="applicantProfile">
+
+    <div id="card">
+        <h2>670<br /><a href="#">Rating Details</a></h2>
+        <a href="#" class="btn">Contact Applicant</a>
+        <img class="profilePic" src="" alt="" />
+        <dl>
+            <dt>Name</dt>
+            <dd>Jon Rundle</dd>
+            <dt>Address</dt>
+            <dd>123 Fake Street</dd>
+            <dt>City</dt>
+            <dd>London</dd>
+            <dt>Province</dt>
+            <dd>Ontario</dd>
+            <dt>Phone</dt>
+            <dd>555-555-5555</dd>
+            <dt>Email</dt>
+            <dd>jon@resolutionim.com</dd>
+            <dt>Links</dt>
+            <dd></dd>
+        </dl>
+        <div id="grade">
+            <h3>Grade Applicant</h3>
+            <a href="#" class="btn green">Recommend</a>
+            <a href="#" class="btn black">Average</a>
+            <a href="#" class="btn black">NQ</a>
+        </div>
+    </div>
+    
+    <div id="submissions">
+        <div id="toolbar">
+            <a class="left" href="#">Back to List</a>
+            <h5><span>Reviewing:</span> Jon Rundle</h5>
+            <span clas="right">
+                <a href="#">Prev</a> // <a href="#">Next</a>
+            </span>
+        </div>
+        <div id="videoResponses">
+            <h3>Video Responses</h3>
+            <ul>
+                <li>
+                    <img src="" alt="" />
+                    <h5>Name of Question</h5>    
+                </li>
+                <li>
+                    <img src="" alt="" />
+                    <h5>Name of Question</h5>    
+                </li>
+                <li>
+                    <img src="" alt="" />
+                    <h5>Name of Question</h5>    
+                </li>
+                <li>
+                    <img src="" alt="" />
+                    <h5>Name of Question</h5>    
+                </li>
+                <li>
+                    <img src="" alt="" />
+                    <h5>Name of Question</h5>    
+                </li>
+            </ul>
+        </div>
+        <div id="resumeCoverLetter">
+            <h3>Resume/Cover Letter</h3>
+            <ul>
+                <li>
+                    <img src="" alt="" />
+                    <h5>Resume</h5>    
+                </li>
+                <li>
+                    <img src="" alt="" />
+                    <h5>Cover Letter</h5>    
+                </li>
+            </ul>
+        </div>
+    </div>
+    
+</section>
