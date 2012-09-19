676b82343d191a11422d3655d7dc2e4946b857a4
Jon Rundle <jon@resolutionim.com>
2012-09-19 13:33:36 -0400

    Sliced Applicant List View

---
+++ includes/apps/applicants/views/applicant-list.php
@@ -0,0 +1,45 @@
+<section id="applicantList">
+    
+    <table>
+        <tr>
+            <th>Applicant Details</th>
+            <th>Intervue Rating</th>
+            <th>Applicant Grade</th>
+        </tr>
+        <tr>
+            <td><div class="imgWrap"><img src="/themes/Intervue/img/profilePicExample.jpg" alt="Applicant's Name" /></div><strong>Jon Rundle</strong><br />London, Ontario</td>
+            <td><h2>670<br /><a href="#">Rating Details</a></h2></td>
+            <td><a href="#" class="btn green">Recommend</a></td>
+        </tr>
+        <tr>
+            <td><div class="imgWrap"><img src="/themes/Intervue/img/profilePicExample.jpg" alt="Applicant's Name" /></div><strong>Andrew Procter</strong><br />Toronto, Ontario</td>
+            <td><h2>580<br /><a href="#">Rating Details</a></h2></td>
+            <td><a href="#" class="btn green">Recommend</a></td>
+        </tr>
+        <tr>
+            <td><div class="imgWrap"><img src="/themes/Intervue/img/profilePicExample.jpg" alt="Applicant's Name" /></div><strong>Jonathan Kochis</strong><br />London, Ontario</td>
+            <td><h2>620<br /><a href="#">Rating Details</a></h2></td>
+            <td><a href="#" class="btn green">Recommend</a></td>
+        </tr>
+        <tr>
+            <td><div class="imgWrap"><img src="/themes/Intervue/img/profilePicExample.jpg" alt="Applicant's Name" /></div><strong>Mike Almond</strong><br />London, Ontario</td>
+            <td><h2>680<br /><a href="#">Rating Details</a></h2></td>
+            <td><a href="#" class="btn green">Recommend</a></td>
+        </tr>
+        <tr>
+            <td><div class="imgWrap"><img src="/themes/Intervue/img/profilePicExample.jpg" alt="Applicant's Name" /></div><strong>Stephanie Johnson</strong><br />London, Ontario</td>
+            <td><h2>630<br /><a href="#">Rating Details</a></h2></td>
+            <td><a href="#" class="btn green">Recommend</a></td>
+        </tr>
+        <tr>
+            <td><div class="imgWrap"><img src="/themes/Intervue/img/profilePicExample.jpg" alt="Applicant's Name" /></div><strong>Evan Fletcher</strong><br />London, Ontario</td>
+            <td><h2>500<br /><a href="#">Rating Details</a></h2></td>
+            <td><a href="#" class="btn green">Recommend</a></td>
+        </tr>
+    </table>
+    
+    <div class="pagination">
+        <a href="#">Prev</a> // <a href="#">Next</a>
+    </div>
+
+</section>
