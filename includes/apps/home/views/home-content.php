0f4212eb95099f466ed6a7189271d7793a676146
Jon Rundle <jon@resolutionim.com>
2012-09-21 14:12:45 -0400

    Home app for content

---
+++ includes/apps/home/views/home-content.php
@@ -0,0 +1,45 @@
+<a href="#" id="priceCallout" class="btn green">First Job is Free! <span>Sign Up Today!</span></a>
+
+<ul id="pricingOptions">
+    <li>
+        <h2>$199</h2>
+        <h5>1 Job</h5>
+        <p>Maecenas faucibus mollis interdum. Cras justo odio, dapibus ac facilisis in, egestas eget quam. Aenean eu leo quam.</p>
+        <p><a class="btn" href="#">Learn More</a></p>
+    </li>
+    <li>
+        <h2>$550</h2>
+        <h5>3 Jobs</h5>
+        <p>Maecenas faucibus mollis interdum. Cras justo odio, dapibus ac facilisis in, egestas eget quam. Aenean eu leo quam.</p>
+        <p><a class="btn" href="#">Learn More</a></p>
+    </li>
+    <li>
+        <h2>$1500</h2>
+        <h5>10 Jobs</h5>
+        <p>Maecenas faucibus mollis interdum. Cras justo odio, dapibus ac facilisis in, egestas eget quam. Aenean eu leo quam.</p>
+        <p><a class="btn" href="#">Learn More</a></p>
+    </li>
+</ul>
+
+<div id="about" class="box">
+    <h3>About Intervue</h3>
+    <p>Maecenas faucibus mollis interdum. Cras justo odio, dapibus ac facilisis in, egestas eget quam. Aenean eu leo quam. Pellentesque ornare sem lacinia quam venenatis vestibulum. Cras mattis consectetur purus sit amet fermentum. Aenean lacinia bibendum nulla sed consectetur. Lorem ipsum dolor sit amet.</p>
+</div>
+
+<div id="testimonials">
+    <div id="testimonialRotator">
+        <div class="testimonialItem">
+            <p>Maecenas faucibus mollis interdum. Cras justo odio, dapibus ac facilisis in, egestas eget quam. Aenean eu leo quam. Pellentesque ornare sem lacinia quam venenatis vestibl.</p>
+            <h4>John Smith<br /><span>Acme, Inc.</span></h4>
+        </div>
+        <div class="testimonialItem">
+            <p>Maecenas faucibus mollis interdum. Cras justo odio, dapibus ac facilisis in, egestas eget quam. Aenean eu leo quam. Pellentesque ornare sem lacinia quam venenatis vestibl.</p>
+            <h4>Jon Rundle<br /><span>Resolution IM</span></h4>
+        </div>
+        <div class="testimonialItem">
+            <p>Maecenas faucibus mollis interdum. Cras justo odio, dapibus ac facilisis in, egestas eget quam. Aenean eu leo quam. Pellentesque ornare sem lacinia quam venenatis vestibl.</p>
+            <h4>Joe Smith<br /><span>Company, Inc.</span></h4>
+        </div>
+    </div>
+    <div id="testimonialNav"></div>
+</div>
