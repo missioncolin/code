21f6f942dabf8b844e474cffb9b054ecaa98b796
Jon Rundle <jon@resolutionim.com>
2012-09-25 11:24:53 -0400

    Changes to the banner

--- includes/apps/banners/views/banners.php
+++ includes/apps/banners/views/banners.php
@@ -1,16 +1,16 @@
 <div id="banner">
     <ul>
         <li>
-            <span>Walk through the job<br />registering process</span>
+            <span><a href="#">Walk through the job<br />registering process</a></span>
         </li>
         <li class="middle">
-            <a class="arrows prev" href="#">Previous</a>
+            <!-- <a class="arrows prev" href="#">Previous</a> -->
             <h2>Why Use<br /><strong>Intervue?</strong></h2>    
             <a href="#">Watch the video! <span class="icon video">Video</span></a>
-            <a class="arrows next" href="#">Next</a>
+            <!-- <a class="arrows next" href="#">Next</a> -->
         </li>
         <li>
-            <span>Walk through the<br />application process</span>
+            <span><a href="#">Walk through the<br />application process</a></span>
         </li>
     </ul>
 </div>
