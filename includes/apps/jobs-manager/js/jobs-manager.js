bed0adf25c3e6e78f79a0d8ff160a6a98dd0ed2c
Karen Laansoo <karen@res.im>
2012-10-03 11:42:32 -0400

    [job manager]
    pop-up dialogs for delete and re-publish

--- includes/apps/jobs-manager/js/jobs-manager.js
+++ includes/apps/jobs-manager/js/jobs-manager.js
@@ -8,21 +8,41 @@ $(function () {
             job: $jobID
         }, function () {
             if ($this.hasClass('grey')) {
-                $this.addClass('black').removeClass('grey').html('Deactivate');
+                $this.addClass('black').removeClass('grey').html('Remove');
             } else {
-                $this.addClass('grey').removeClass('black').html('Activate/publish');
+                $this.addClass('grey').removeClass('black').html('Publish');
             }
         });
     });
     
+    var clearPopUp = function(){
+        $('#confirm').fadeOut();
+        $('.popUp h2').empty();
+        $('.popUp p').empty();
+        $('.popUp #popUpOk').off('click');
+        $('.popUp #popUpNo').off('click');
+    }
+    
+    var confirmAction = function(title, message){
+        $('.popUp h2').html(title);
+        $('.popUp p').html(message);
+        $('#confirm').fadeIn();
+    }
+
     
     $('.delete').click(function () {
         var $jobID = $(this).data('job');
         var $this = $(this);
-        $.post('/delete-job', {
-            job: $jobID
-        }, function() {
-            $this.parent().parent().remove();
+        confirmAction("Delete Job?", "Once this job is deleted, you will no longer be able to edit it, or view applications");
+        $('.popUp #popUpNo').on('click', clearPopUp);
+        $('.popUp #popUpOk ').on('click', function(){
+            $.post('/delete-job', {
+                job: $jobID
+            }, function() {
+                $this.parent().parent().remove();
+                $('.alert').removeClass('fail').addClass('success').html('<span></span>Job Deleted Successfully');
+            });
+            clearPopUp();
         });
     }); 
     
@@ -44,50 +64,77 @@ $(function () {
     
     $('.reactivate').click(function() {
         var $jobID = $(this).data('job');
-        var $this = $(this);
-        var parTD = $(this).parent();
-        var parTR = $(this).parents('tr').index();
-        $.post('/reactivate-job', {
-            job: $jobID
-        }, function(data) {
-            console.log(data);
-            if (data == 'success'){
-                //create active elements
-                var credits = $('#loggedInButtons a:eq(0)').html().match(/^(\d+)\sCredits$/);
-                if (typeof credits != 'undefined' && credits[1] > 0){
-                    var creditHTML = (parseInt(credits[1], 10) - 1)+' Credits';
-                    $('#loggedInButtons a:eq(0)').html(creditHTML);
+        var react = $(this);
+        confirmAction("Re-Publish Job", "Re-publishing this job will cost one (1) credit");
+        $('.popUp #popUpNo').on('click', clearPopUp);
+        $('.popUp #popUpOk ').on('click', function(){
+            var parTD = react.parent();
+            var parTR = react.parents('tr').index();
+            $.post('/reactivate-job', {
+                job: $jobID
+            }, function(data) {
+                clearPopUp();
+                if (data == 'success'){
+                    //create active elements
+                    var credits = $('#loggedInButtons a:eq(0)').html().match(/^(\d+)\sCredits$/);
+                    if (typeof credits != 'undefined' && credits[1] > 0){
+                        var creditHTML = (parseInt(credits[1], 10) - 1)+' Credits';
+                        $('#loggedInButtons a:eq(0)').html(creditHTML);
+                        
+                        var domTR = document.getElementById('hrListJobs').getElementsByTagName('tr');
+                        var actTD = domTR[parTR].getElementsByTagName('td');
+                        
+                        parTD.attr('colspan','1');
+                        $('a', parTD).removeClass('green').removeClass('reactivate').addClass('black').addClass('activate').html('Remove');
+                        
+                        var tdLink = document.createElement('td');
+                        var linkText = document.createTextNode(location.host+"/apply/"+$jobID);
+                        tdLink.appendChild(linkText);
+                        
+                        var tdDel = document.createElement('td');
+                        var aDel = document.createElement('a');
+                        aDel.setAttribute('href', '#');
+                        aDel.setAttribute('data-job', $jobID);
+                        aDel.className = "btn red delete";
+                        var delText = document.createTextNode('Delete');
+                        aDel.appendChild(delText);
+                        tdDel.appendChild(aDel);
+                        var tdEdit = document.createElement('td');
+                        var aEdit = document.createElement('a');
+                        aEdit.setAttribute('href', '/edit-job?id='+$jobID);
+                        aEdit.className = "btn";
+                        var editText = document.createTextNode('Edit');
+                        aEdit.appendChild(editText);
+                        tdEdit.appendChild(aEdit);
+                        
+                        domTR[parTR].insertBefore(tdLink, actTD[1]);
+                        domTR[parTR].appendChild(tdDel);
+                        domTR[parTR].appendChild(tdEdit);
+                        $('.alert').removeClass('fail').addClass('success').html('<span></span>Job Re-published Successfully. Your account was debited one (1) credit');
+                    }
                     
-                    var domTR = document.getElementById('hrListJobs').getElementsByTagName('tr');
-                    
-                    parTD.attr('colspan','1');
-                    $('a', parTD).removeClass('green').removeClass('reactivate').addClass('black').addClass('activate').html('Deactivate');
-                    
-                    var tdDel = document.createElement('td');
-                    var aDel = document.createElement('a');
-                    aDel.setAttribute('href', '#');
-                    aDel.setAttribute('data-job', $jobID);
-                    aDel.className = "btn red delete";
-                    var delText = document.createTextNode('Delete');
-                    aDel.appendChild(delText);
-                    tdDel.appendChild(aDel);
-                    var tdEdit = document.createElement('td');
-                    var aEdit = document.createElement('a');
-                    aEdit.setAttribute('href', '/edit-job?id='+$jobID);
-                    aEdit.className = "btn";
-                    var editText = document.createTextNode('Edit');
-                    aEdit.appendChild(editText);
-                    tdEdit.appendChild(aEdit);
-                    
-                    domTR[parTR].appendChild(tdDel);
-                    domTR[parTR].appendChild(tdEdit);
-                    //success message in a dialog box of some kind
                 }
-                
-            }
-            else{
-                //error message in a dialog box of some kind
-            }
+                else{
+                    $('.alert').removeClass('success').addClass('fail').html('<span></span>Job not Re-published. '+data);
+                }
+                clearPopUp();
+            });
+            
+        });
+        
+    });
+    
+    $('.buy').click(function(e){
+        e.preventDefault();
+        var $jobID = $(this).data('job');
+        var react = $(this);
+        confirmAction("Re-Publish Job", "You do not have enough credits. Re-publishing this job will cost one (1) credit.<br />Do you want to buy credits?");
+        $('.popUp #popUpNo').on('click', function(){
+            clearPopUp();
+        });
+        $('.popUp #popUpOk ').on('click', function(){
+            clearPopUp();
+            window.location.href = react.attr('href');
         });
     });
    
