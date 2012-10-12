a3311bff960c2b019649525b44bf8ac5db045e58
Jon Rundle <jon@resolutionim.com>
2012-10-12 15:53:07 -0400

    new job alert changed to disabled

--- includes/apps/jobs-manager/js/jobs-manager.js
+++ includes/apps/jobs-manager/js/jobs-manager.js
@@ -53,7 +53,7 @@ $(function () {
         var $this = $(this);
         var link = $('.newJobAlert').text();
         $('.popUp').addClass('success');
-        confirmAction("Your Job Has Been Created!", "Use this link to send applicants to your job: <br /><form><input class='jobAlertInput' type='text' value='http://" + link + "' /></form>");
+        confirmAction("Your Job Has Been Created!", "Use this link to send applicants to your job: <br /><form><input class='jobAlertInput' type='text' disabled='disabled' value='http://" + link + "' /></form>");
         $('.jobAlertInput').focus();
         $('.jobAlertInput').select();
         $('.popUp #popUpNo').hide();
