e3b4137016404f779fb548e4b5e9f981c67a5d14
Evan Fletcher <evan@res.im>
2013-03-04 10:55:42 -0500

    Home page slicing Continued.. Icons and screenshots added

--- includes/apps/home/views/home-content.php
+++ includes/apps/home/views/home-content.php
@@ -2,22 +2,23 @@
 		<h2>Hire the Best Talent in 3 Easy Steps</h2>
 	    <ol>
 	        <li>
-	        	<i class="icon"></i>
-	            <h4>Each Application Includes</h4><br />
+	        	<i class="icon"><img src="../../../../themes/Intervue/img/linkIcon.png" alt="linkIcon" width="110" height="84" /></i>
+	            <h4>Set up your joblink</h4><br />
 	            <p>Create a job and intervue will generate a specific for your job</p>
 	        </li>
 	        <li>
-	        	<i class="icon"></i>
-	            <h4>Each Application Includes</h4><br />
-	            <p>Create a job and intervue will generate a specific for your job</p>
+	        	<i class="icon"><img src="../../../../themes/Intervue/img/laptopIcon.png" alt="laptopIcon" width="110" height="84" /></i>
+	            <h4>Post your job on any site</h4><br />
+	            <p>Include the joblink in your posting and direct applicants to click the link to apply</p>
 	        </li>
 	        <li>
-	        	<i class="icon"></i>
-	            <h4>Each Application Includes</h4><br />
-	            <p>Create a job and intervue will generate a specific for your job</p>
+	        	<i class="icon"><img src="../../../../themes/Intervue/img/mouseIcon.png" alt="mouseIcon" width="110" height="84" /></i>
+	            <h4>Click joblink to apply</h4><br />
+	            <p>Applicants are guided through the application process and apply for you job opening</p>
 	        </li>
 	        <div class="clearfix"></div>
 	    </ol>
+	    <div class="cta"><a href="/create-a-job" class="startBtn">Start</a><div class="badge"></div></div>
 	</div>
 	 
 	<hr>
@@ -26,28 +27,28 @@
 	
 	<div id="sectionOne">
 		
-		<h2>See How it Works<i class="icon arrow"></i></h2>
+		<h2>See How it Works<i class="icon arrowCurved"><img src="../../../../themes/Intervue/img/arrowCurved.png" alt="arrowCurved" width="87" height="76" /></i></h2>
 	
 		<div class="videoBox"></div>
 	
 		<ul>
 		    <li>
-		    	<i class="icon breifCase"></i>
+		    	<i class="icon breifCase"><img src="../../../../themes/Intervue/img/breifCaseIcon.png" alt="breifCaseIcon" width="120" height="120" /></i>
 		        <h4>Manage everything</h4>
 		        <p>Applicants apply though intervue and all their information is stored and organized in one place</p>
 		    </li>
 		    <li>
-		    	<i class="icon certificate"></i>
+		    	<i class="icon certificate"><img src="../../../../themes/Intervue/img/certificateIcon.png" alt="certificateIcon" width="120" height="120" /></i>
 		        <h4>Grades Applicants</h4>
 		        <p>Tell us what skills you are looking for and intervue will grade applicants for you</p>
 		    </li>
 		    <li>
-		    	<i class="icon peoples"></i>
+		    	<i class="icon peoples"><img src="../../../../themes/Intervue/img/peopleIcon.png" alt="peopleIcon" width="120" height="120" /></i>
 		        <h4>Screens them too...</h4>
 		        <p>Use filters to display only those who have the right skills and experience for the job</p>
 		    </li>
 		    <li>
-		    	<i class="icon interviewers"></i>
+		    	<i class="icon interviewers"><img src="../../../../themes/Intervue/img/interviewIcon.png" alt="intervueIcon" width="120" height="120" /></i>
 		        <h4>...and does Interviews</h4>
 		        <p>Get to know the candidates by watching their recorded answers to your questions</p>
 		    </li>
@@ -67,12 +68,12 @@
 		    	<p>Name your job, list the skills and experience you are looking for and ask the interview questions you want answered</p>
 	    	</li>
 	    	<li>
-	    		<img/>
+	    		<img src="../../../../themes/Intervue/img/addJobScreenShot.png" alt="addJobScreenShot" width="280" />
 	    	</li>
 	    </ul>
 	    <ul class="imgLeft">
 	    	<li>
-	    		<img/>
+	    		<img src="../../../../themes/Intervue/img/publishedLinkScreenShot.png" alt="publishedLinkScreenShot" width="280" height="158" />
 	    	</li>
 	    	<li>
 		    	<h4>Activate your joblink</h4>
@@ -85,12 +86,12 @@
 		    	<p>Post your job using your normal job sites. Include the joblink in the posting and direct applicants to click it to apply</p>
 	    	</li>
 	    	<li>
-	    		<img/>
+	    		<img src="../../../../themes/Intervue/img/otherJobSites.png" alt="otherJobSites" width="318" height="123" />
 	    	</li>
 	    </ul>
 	    <ul class="imgLeft">
 	    	<li>
-	    		<img/>
+	    		<img src="../../../../themes/Intervue/img/applyScreenShot.png" alt="applyScreenShot" width="280" height="160" />
 	    	</li>
 	    	<li>
 		    	<h4>Activate through Intervue</h4>
@@ -103,12 +104,12 @@
 		    	<p>All applications are stored and organized in one place. Access all their information with a single click</p>
 	    	</li>
 	    	<li>
-	    		<img/>
+	    		<img src="../../../../themes/Intervue/img/applicantsScreenShot.png" alt="applicantsScreenShot" width="280" height="95" />
 	    	</li>
 	    </ul>
 	    <ul class="imgLeft">
 	    	<li>
-	    		<img/>
+	    		<p style="font-size: 60px;padding: 10px 0 0 0;">100%</p>
 	    	</li>
 	    	<li>
 		    	<h4>Grade Applicants</h4>
@@ -121,12 +122,12 @@
 		    	<p>Use the skills sliders so you can find the candidates with right skills to fit your job</p>
 	    	</li>
 	    	<li>
-	    		<img/>
+	    		<img src="../../../../themes/Intervue/img/experienceSliderScreenShot.png" alt="experienceSliderScreenShot" width="280" height="214" />
 	    	</li>
 	    </ul>
 	    <ul class="imgLeft">
 	    	<li>
-	    		<img/>
+	    		<img src="../../../../themes/Intervue/img/videoScreenShot.png" alt="videoScreenShot" width="220" height="160" />
 	    	</li>
 	    	<li>
 		    	<h4>...and does Interviews</h4>
@@ -139,12 +140,12 @@
 		    	<p>Tag your applicants as top candidate or has potential so you can separate the best from the rest.</p>
 	    	</li>
 	    	<li>
-	    		<img/>
+	    		<img src="../../../../themes/Intervue/img/topCandidateScreenShot.png" alt="topCandidateScreenShot" width="280" height="160" />
 	    	</li>
 	    </ul>
 	    <ul class="imgLeft">
 	    	<li>
-	    		<img/>
+	    		<img src="../../../../themes/Intervue/img/handshakeIcon.png" alt="handshakeIcon" width="120" height="120" />
 	    	</li>
 	    	<li>
 		    	<h4>Hire the Best Employee Fast</h4>
@@ -152,7 +153,36 @@
 	    	</li>
 	    </ul>
 	    <div class="clearfix"></div>
+	     <div class="cta"><a href="/create-a-job" class="startBtn">Start</a><div class="badge"></div></div>
 	</div>
-
+	
+	<hr>
+	
+	<div id="newFooter">
+		<ul>
+			<h3>Features</h3>
+			<li><a href="">Manages</a></li>
+			<li><a href="">Grades</a></li>
+			<li><a href="">Screens</a></li>
+			<li><a href="">Interviews</a></li>
+		</ul>
+		<ul>
+			<h3>Community</h3>
+			<li><a href="">Twitter</a></li>
+			<li><a href="">Facebook</a></li>
+			<li><a href="">LinkIn</a></li>
+			<li><a href="">YouTube</a></li>
+		</ul>
+		<ul>
+			<h3 class="companyName">Intervue</h3>
+			<li><a href="">About Us</a></li>
+			<li><a href="">How it Works</a></li>
+			<li><a href="">Contact Us</a></li>
+			<li><a href="">Sign In</a></li>
+			<li><a href="">Sign Up</a></li>
+			<li><a href="">FAQs</a></li>
+		</ul>
+		<div class="clearfix"></div>
+	</div>
 
 </div>
