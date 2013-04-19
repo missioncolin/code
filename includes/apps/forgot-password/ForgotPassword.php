264a7825107f0acade2ed53cb3495274a02ed735
Mike Almond <mike@res.im>
2013-04-19 10:13:12 -0400

    Password reset did not look for valid users and only sent to one account attached to an email
    ref #152

--- includes/apps/forgot-password/ForgotPassword.php
+++ includes/apps/forgot-password/ForgotPassword.php
@@ -3,7 +3,7 @@
 class ForgotPassword {
 
     var $db;
-    
+
     public function __construct($db) {
         $this->db = $db;
     }
@@ -28,18 +28,18 @@ class ForgotPassword {
         $qry = sprintf("SELECT itemID FROM sysUsers WHERE `%s` = '%s'",
             $this->db->escape($type),
             $this->db->escape($token));
-            
+
         $res = $this->db->query($qry);
 
         if ($this->db->num_rows($res) == 1) {
             list ($id) = $this->db->fetch_array($res);
             return new User($this->db, $id);
         }
-        
+
         throw new Exception("Invalid token");
     }
 
-    
+
     /**
      * @return string
      */
@@ -56,54 +56,67 @@ class ForgotPassword {
     public function sendReset($email) {
 
         if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
-            return false;
+            return 'Invalid email address';
         }
 
 
         // check to see if the email exists
-        $qry = sprintf("SELECT userID FROM sysUGFValues WHERE sysStatus='active' AND sysOpen='1' AND fieldID='3' AND value='%s'",
+        $qry = sprintf("SELECT uv.userID
+            FROM sysUGFValues AS uv
+            INNER JOIN sysUsers AS u ON u.itemID=uv.userID
+            WHERE uv.sysStatus='active'
+            AND uv.sysOpen='1'
+            AND u.sysStatus='active'
+            AND u.sysOpen='1'
+            AND uv.fieldID='3'
+            AND uv.value='%s'",
             $this->db->escape($email));
         $res = $this->db->query($qry);
         if ($this->db->num_rows($res) > 0) {
 
-            list($userID) = $this->db->fetch_array($res);
-
-            // insert a unique hash into the DB to check on later
-            $hash = $this->generateToken();
-            $qry = sprintf("UPDATE sysUsers SET fpHash='%s' WHERE itemID='%d'",
-                $hash,
-                $userID);
-            $this->db->query($qry);
-
-            $resetLink = 'http://' . $_SERVER['HTTP_HOST'] . '/reset-password?token=' . $hash;
-
             $mail = new PHPMailer();
 
-            $mail->Subject = ucwords($_SERVER['HTTP_HOST']) . " password reset";
-            $mail->AddAddress($email);
-            $mail->SetFrom('no-reply@intervue.ca', 'Intervue');
+            while ($u = $this->db->fetch_assoc($res)) {
+
+                $hash = $this->generateToken();
+
+                // insert a unique hash into the DB to check on later
+                $qry = sprintf("UPDATE sysUsers SET fpHash='%s' WHERE itemID='%d'",
+                    $hash,
+                    $u['userID']);
+                $this->db->query($qry);
+                $resetLink = 'http://' . $_SERVER['HTTP_HOST'] . '/reset-password?token=' . $hash;
+
+                $mail->Subject = ucwords($_SERVER['HTTP_HOST']) . " password reset";
+                $mail->AddAddress($email);
+                $mail->SetFrom('no-reply@intervue.ca', 'Intervue');
 
 
-            $mail->Body = "<p>Hi there,</p>
+                $mail->Body = "<p>Hi there,</p>
 
-<p>There was recently a request to change the password on your account.</p>
+    <p>There was recently a request to change the password on your account.</p>
 
-<p>If you requested this password change, please set a new password by following the link below:</p>
+    <p>If you requested this password change, please set a new password by following the link below:</p>
 
-<p>{$resetLink}</p>
+    <p>{$resetLink}</p>
 
-<p>If you don't want to change your password, just ignore this message.</p>
+    <p>If you don't want to change your password, just ignore this message.</p>
 
-<p>Thanks</p>";
+    <p>Thanks</p>";
 
-            $mail->AltBody = strip_tags($mail->Body);
-            
-            return $mail->Send();
-        } else {
-            return 'No user found';
+                $mail->AltBody = strip_tags($mail->Body);
+
+                if (!$mail->Send()) {
+                    return false;
+                }
+
+                $mail->ClearAllRecipients();
+
+            }
+
+            return true;
         }
 
-        return false;
+        return 'No user found';
     }
-
 }
